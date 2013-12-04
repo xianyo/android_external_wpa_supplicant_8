6dc847c32c3c26cc9f162782494d8b6117c9b78f
Jianzheng Zhou <jianzheng.zhou@freescale.com>
2013-12-04 11:40:42 +0800

    ENGR00290615 Wifi:enable wifi on KK4.4
    Change to use android private socket instead of UNIX standard socket.
    For AOSP only update to use single connection to transfer commands
    and events based on android private socket.
    
    Signed-off-by: Jianzheng Zhou <jianzheng.zhou@freescale.com>

--- wpa_supplicant/Android.mk
+++ wpa_supplicant/Android.mk
@@ -1620,7 +1620,7 @@ include $(BUILD_EXECUTABLE)
 #include $(BUILD_EXECUTABLE)
 #
 ########################
-
+ifeq ($(WPA_SUPPLICANT_VERSION),$(filter $(WPA_SUPPLICANT_VERSION),VER_0_8_UNITE))
 local_target_dir := $(TARGET_OUT)/etc/wifi
 
 include $(CLEAR_VARS)
@@ -1629,7 +1629,7 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
-
+endif
 ########################
 
 include $(CLEAR_VARS)
