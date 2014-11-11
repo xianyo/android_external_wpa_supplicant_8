7b4abfcebec8188b81c0b355e170d06b14a19a77
Wang Haoran <b50027@freescale.com>
2014-11-11 10:02:48 +0800

    MA-5839-2 Fix compile error for upgrade Android-L
    Choose the correct libnl for wpa_supplicant private
    lib.
    
    Signed-off-by: Wang Haoran <b50027@freescale.com>

--- wpa_supplicant/Android.mk
+++ wpa_supplicant/Android.mk
@@ -1597,7 +1597,11 @@ ifeq ($(CONFIG_TLS), openssl)
 LOCAL_SHARED_LIBRARIES += libcrypto libssl libkeystore_binder
 endif
 ifdef CONFIG_DRIVER_NL80211
+ifneq ($(wildcard external/libnl),)
+LOCAL_SHARED_LIBRARIES += libnl
+else
 LOCAL_STATIC_LIBRARIES += libnl_2
+endif
 endif
 LOCAL_CFLAGS := $(L_CFLAGS)
 LOCAL_CFLAGS += -DREALTEK_WIFI_VENDOR
