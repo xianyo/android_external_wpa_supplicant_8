f934a4968222c1ee70761fbefc5aa1f2d6e99be9
Wang Haoran <b50027@freescale.com>
2014-11-19 20:29:16 +0800

    MA-5881 P2p use 2.4G only
    As the legacy device cannot use 5G as p2p
    channel we filter the non-2.4G channel when
    update the channel list.
    
    Signed-off-by: Wang Haoran <b50027@freescale.com>

--- wpa_supplicant/p2p_supplicant.c
+++ wpa_supplicant/p2p_supplicant.c
@@ -3631,6 +3631,8 @@ static int wpas_p2p_setup_channels(struct wpa_supplicant *wpa_s,
 
 	for (op = 0; op_class[op].op_class; op++) {
 		struct p2p_oper_class_map *o = &op_class[op];
+        if (o->op_class != 81)
+                continue;
 		u8 ch;
 		struct p2p_reg_class *reg = NULL, *cli_reg = NULL;
 
